428111bc4675514e515de04436aa5d1ab02e08ff
hari23t <loveharihari246@gmail.com>
2025-11-18 09:06:54 -0800

    hrllo

--- sum.c
+++ sum.c
@@ -2,12 +2,5 @@
 void main()
 {
   int n,sum=0;
-  printf("Enter n:");
-  scanf("%d",&n);
-  while(n>0)
- {
-   sum=sum+(n%10);
-   n=n/10;
- }
- printf("sum of digits =%d",sum);
+ print aa:'al
 }   
